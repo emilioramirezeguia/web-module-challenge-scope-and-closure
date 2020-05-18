cb8b604481784ae6563a900a4edb86c4a7fb9def
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-05-18 14:22:58 -0500

    Complete task 1

--- index.js
+++ index.js
@@ -2,44 +2,54 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
-// ⭐️ Example Challenge END ⭐️
+processFirstItem(["telon", "mexico"], callback);
 
+// ⭐️ Example Challenge END ⭐️
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+
+ The count variable from counter1 can only be accessed from inside the counterMaker function. In counter2, the count variable can be accessed from anywhere since it's defined globally.
+
+ *
+ * 2. Which of the two uses a closure? How can you tell?
+
+ counter1 uses closure since it has a function within a function.
+
+ *
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+
+ counter1 would be preferable when we want to keep an updated value for count that we can track or modify at any time. couter2 would be preferable to run something that doesn't require long-term memory, like a for loop.
+ 
+ *
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,40 +61,35 @@ function counter2() {
   return count++;
 }
 
-
-/* Task 2: inning() 
+/* Task 2: inning()
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(/*Code Here*/) {
+  /*Code Here*/
 }
 
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
 
-For example, 
+For example,
 
-finalScore(inning, 9) might return: 
+finalScore(inning, 9) might return:
 {
   "Home": 11,
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-
+function finalScore(/*code Here*/) {
   /*Code Here*/
-
 }
 
-/* Task 4: 
+/* Task 4:
 
-Create a function called `scoreboard` that accepts the following parameters: 
+Create a function called `scoreboard` that accepts the following parameters:
 
 (1) Callback function `inning` that you wrote above
 (2) A number of innings
@@ -106,5 +111,3 @@ Final Score: 6 - 10 */
 function scoreboard(/* CODE HERE */) {
   /* CODE HERE */
 }
-
-
